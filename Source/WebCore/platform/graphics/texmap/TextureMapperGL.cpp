404b002ee0ce89f63255e1b5a67ed893726397ca
Miguel Gomez <magomez@igalia.com>
2015-11-05 14:59:04 +0100

    Remove the RGBA restriction when using Qt

--- Source/WebCore/platform/graphics/texmap/TextureMapperGL.cpp
+++ Source/WebCore/platform/graphics/texmap/TextureMapperGL.cpp
@@ -839,11 +839,7 @@ void BitmapTextureGL::didReset()
             externalFormat = GraphicsContext3D::RGBA;
     }
 
-#if PLATFORM(QT)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_textureSize.width(), m_textureSize.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
-#else
     m_context3D->texImage2DDirect(GraphicsContext3D::TEXTURE_2D, 0, internalFormat, m_textureSize.width(), m_textureSize.height(), 0, externalFormat, DEFAULT_TEXTURE_PIXEL_TRANSFER_TYPE, 0);
-#endif
 }
 
 void BitmapTextureGL::updateContentsNoSwizzle(const void* srcData, const IntRect& targetRect, const IntPoint& sourceOffset, int bytesPerLine, unsigned bytesPerPixel, Platform3DObject glFormat)
